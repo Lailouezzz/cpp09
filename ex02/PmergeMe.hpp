c19537fc04ab3c9a71267b9cc4ee36135af2f11f
Lailouezzz <alanlebouder@gmail.com>
2024-10-01 16:10:49 +0200

    caca

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -5,20 +5,78 @@
 # include <vector>
 
 template<class T>
-void	_sortPairs(T& container)
+void	_sortPairs(T& container, std::size_t unit)
 {
-	if (container.size() < 2) return ;
+	if (container.size() < unit * 2) return ;
 
-	for (std::size_t k = 0; k + 1 < container.size(); k += 2)
-		if (container[k] > container[k + 1])
-			std::swap(container[k], container[k + 1]);
+	for (std::size_t k = 0; k + unit * 2 - 1 < container.size(); k += unit * 2)
+		if (container[k + unit - 1] > container[k + unit * 2 - 1])
+			std::swap_ranges(container.begin()+k, container.begin()+k+unit, container.begin()+k+unit);
 }
 
 template<class T>
-void	fjmi(T& container)
+T	_mainChain(T& container, std::size_t unit)
 {
-	// std::deque<int>	container;
-	_sortPairs(container);
+	// if (container.size() < unit * 2) return ;
+	T	main_chain;
+
+	for (std::size_t k = 0; k + unit * 2 - 1 < container.size(); k += unit * 2)
+		main_chain.insert(main_chain.end(), container.begin()+k+unit, container.begin()+k+unit*2);
+	return (main_chain);
+}
+
+template<class T>
+T	_pendingChain(T& container, std::size_t unit)
+{
+	// if (container.size() < unit * 2) return ;
+	T	pending_chain;
+
+	for (std::size_t k = 0; k + unit * 2 - 1 < container.size(); k += unit * 2)
+		pending_chain.insert(pending_chain.end(), container.begin()+k, container.begin()+k+unit);
+	// Leave extra elements
+	pending_chain.insert(pending_chain.end(), container.begin()+(unit*2*(container.size()/(unit*2))), container.end());
+	return (pending_chain);
+}
+
+template<class T>
+void	_binaryInsertion(T& main_chain, typename T::const_iterator itbeg, typename T::const_iterator itend, std::size_t unit)
+{
+	int	b = *(itend-1);
+	std::size_t	start = 0;
+	std::size_t	end = (main_chain.size() / unit);
+
+	while (start != end)
+	{
+		std::size_t	middle = (start + end) / 2;
+
+		if (b > main_chain[middle*unit+unit-1])
+			start = middle + 1;
+		else
+			end = middle;
+	}
+	main_chain.insert(main_chain.begin()+start*unit, itbeg, itend);
+}
+
+template<class T>
+void	_insertPairs(T& container, T const& pending_chain, std::size_t unit)
+{
+	for(std::size_t k = 0; k + unit - 1 < pending_chain.size(); k += unit)
+		_binaryInsertion<T>(container, pending_chain.begin()+k, pending_chain.begin()+k+unit, unit);
+	// Insert remaining elements
+	container.insert(container.end(), pending_chain.begin()+(unit*(pending_chain.size()/unit)), pending_chain.end());
+}
+
+template<class T>
+void	fjmi(T& container, std::size_t unit=1)
+{
+	if (container.size() / unit <= 1) return ;
+	_sortPairs(container, unit);
+	fjmi<T>(container, unit * 2);
+
+	// Binary insertion
+	T	pending_chain(_pendingChain<T>(container, unit));
+	container = _mainChain<T>(container, unit);
+	_insertPairs<T>(container, pending_chain, unit);
 }
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,32 +1,57 @@
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
 
 #include "PmergeMe.hpp"
 
-
-int main(void)
+int to_int(std::string const& ratio)
 {
-	std::deque<int>	container;
+	std::stringstream	ss;
+	int				r;
 
-	container.push_back(1);
-	container.push_back(7);
-	container.push_back(6);
-	container.push_back(4);
-	container.push_back(5);
-	container.push_back(2);
-	container.push_back(3);
+	ss << ratio.substr(0, ratio.find_last_not_of(' ') + (ratio.find_last_not_of(' ') != std::string::npos ? 1 : 0));
+	if (!(ss >> r) || !ss.eof())
+		throw std::exception();
+	return (r);
+}
 
-	std::deque<int>::const_iterator	itr = container.begin();
-	while (itr != container.end())
+#define NUM_DISPLAYED_ITEMS (5)
+
+template<class T>
+void	printTab(T const& container)
+{
+	typename T::const_iterator itr = container.begin();
+	for (int k = 0; k < NUM_DISPLAYED_ITEMS && itr != container.end(); ++k, ++itr)
+		std::cout << " " << *itr;
+}
+
+int main(int argc, char **argv)
+{
+	std::deque<int>	deque;
+	std::vector<int>	vector;
+	for (int k = 1; k < argc; ++k)
 	{
-		std::cout << *itr << std::endl;
-		++itr;
+		int	t;
+		try
+		{
+			t = to_int(argv[k]);
+		}
+		catch (std::exception const& e)
+		{
+			std::cerr << *argv << ": " << argv[k] << " is not an integer" << std::endl;
+			return (EXIT_FAILURE);
+		}
+		deque.push_back(t);
+		vector.push_back(t);
 	}
-	fjmi<std::deque<int> >(container);
-	itr = container.begin();
-	std::cout << "----" << std::endl;
-	while (itr != container.end())
-	{
-		std::cout << *itr << std::endl;
-		++itr;
-	}
+	std::cout << "Before :";
+	printTab<std::vector<int> >(vector);
+	std::cout << std::endl;
+	fjmi<std::deque<int> >(deque);
+	fjmi<std::vector<int> >(vector);
+	std::cout << "After :";
+	printTab<std::vector<int> >(vector);
+	std::cout << std::endl;
+	
+	return (EXIT_SUCCESS);
 }
